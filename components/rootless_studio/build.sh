7a2d1dacb444cd3ee6616b1e8621d089004e9671
Yauhen Artsiukhou <jsirex@gmail.com>
2019-08-26 14:14:43 +0300

    Allow rootless studio to accept bio version during build

---
+++ components/rootless_studio/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -e
+
+BIO_VERSION="${1:-0.85.0}"
+
+docker build --no-cache --builder-arg BIO_VERSION="$BIO_VERSION" -t biomesh/bio:"$BIO_VERSION" .
+docker build --no-cache --builder-arg BIO_VERSION="$BIO_VERSION" -t biomesh/default-studio-x86_64-linux:"$BIO_VERSION" ./default
