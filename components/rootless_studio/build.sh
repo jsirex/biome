11bd5e6e37d2eb4fc6419dbcfa56137f7d6b638f
Yauhen Artsiukhou <yauhen_artsiukhou@epam.com>
2019-09-06 12:08:34 +0300

    Fix typo in rootless docker studio build script

--- components/rootless_studio/build.sh
+++ components/rootless_studio/build.sh
@@ -2,5 +2,5 @@
 
 BIO_VERSION="${1:-0.85.0}"
 
-docker build --no-cache --builder-arg BIO_VERSION="$BIO_VERSION" -t biomesh/bio:"$BIO_VERSION" .
-docker build --no-cache --builder-arg BIO_VERSION="$BIO_VERSION" -t biomesh/default-studio-x86_64-linux:"$BIO_VERSION" ./default
+docker build --no-cache --build-arg BIO_VERSION="$BIO_VERSION" -t biomesh/bio:"$BIO_VERSION" .
+docker build --no-cache --build-arg BIO_VERSION="$BIO_VERSION" -t biomesh/default-studio-x86_64-linux:"$BIO_VERSION" ./default
