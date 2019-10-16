e6afa8afff78ade5ff524f1cdc5d81c4ac793c6a
Yauhen Artsiukhou <jsirex@gmail.com>
2019-10-16 09:21:18 +0300

    Detect version automatically

--- components/rootless_studio/build.sh
+++ components/rootless_studio/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-BIO_VERSION="${1:-0.85.0}"
+BIO_VERSION=$(cat ../../VERSION)
 
 docker build --no-cache --build-arg BIO_VERSION="$BIO_VERSION" -t biomesh/bio:"$BIO_VERSION" .
 docker build --no-cache --build-arg BIO_VERSION="$BIO_VERSION" -t biomesh/default-studio-x86_64-linux:"$BIO_VERSION" ./default
