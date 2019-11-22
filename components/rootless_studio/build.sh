a63dc283fa06c9992cc4476cad1a69661ce27f58
Yauhen Artsiukhou <jsirex@gmail.com>
2019-11-22 15:55:57 +0300

    Introduce TARGET when building docker studio

--- components/rootless_studio/build.sh
+++ components/rootless_studio/build.sh
@@ -1,6 +1,18 @@
 #!/bin/sh -e
 
 BIO_VERSION=$(cat ../../VERSION)
+BIO_TARGET=x86_64-linux
 
-docker build --no-cache --build-arg BIO_VERSION="$BIO_VERSION" -t biomesh/bio:"$BIO_VERSION" .
-docker build --no-cache --build-arg BIO_VERSION="$BIO_VERSION" -t biomesh/default-studio-x86_64-linux:"$BIO_VERSION" ./default
+docker build \
+       -t biomesh/bio-"$BIO_TARGET":"$BIO_VERSION" \
+       --no-cache \
+       --build-arg BIO_VERSION="$BIO_VERSION" \
+       --build-arg BIO_TARGET="$BIO_TARGET" \
+       .
+
+docker build \
+       -t biomesh/default-studio-"$BIO_TARGET":"$BIO_VERSION" \
+       --no-cache \
+       --build-arg BIO_VERSION="$BIO_VERSION" \
+       --build-arg BIO_TARGET="$BIO_TARGET" \
+       ./default
